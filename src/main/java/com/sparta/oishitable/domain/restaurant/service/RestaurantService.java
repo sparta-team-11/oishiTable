cae3c38365d123fddac3962fcc4b69c69f4bdad7
gkdl4239 <gkdl4239@gmail.com>
2025-02-12 20:13:15 +0900

    :sparkles: feat: 레포지토리 참조

--- src/main/java/com/sparta/oishitable/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/sparta/oishitable/domain/restaurant/service/RestaurantService.java
@@ -1,5 +1,6 @@
 package com.sparta.oishitable.domain.restaurant.service;
 
+import com.sparta.oishitable.domain.restaurant.repository.RestaurantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
 }
