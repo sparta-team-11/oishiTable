c723474c3c5bb065d661881a4a9ab823f9b07c4d
SeuneKeun <eiziong1@gmail.com>
2025-02-12 17:49:51 +0900

    :sparkles: feat: pr 템플릿 추가

---
+++ src/main/java/com/sparta/oishitable/domain/restaurantseat/service/RestaurantSeatService.java
@@ -0,0 +1,11 @@
+package com.sparta.oishitable.domain.restaurantseat.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RestaurantSeatService {
+}
