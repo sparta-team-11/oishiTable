2fdab3bd9f0c146a81f57c11e385c5b47ae57055
legoChoi <chlx159@gmail.com>
2025-02-13 15:48:38 +0900

    :art: refactor: 가게 등록 API 수정
    - 불필요한 스트림 제거

--- src/main/java/com/sparta/oishitable/domain/restaurantseat/service/RestaurantSeatService.java
+++ src/main/java/com/sparta/oishitable/domain/restaurantseat/service/RestaurantSeatService.java
@@ -4,14 +4,12 @@ import com.sparta.oishitable.domain.restaurant.entity.Restaurant;
 import com.sparta.oishitable.domain.restaurantseat.dto.request.RestaurantSeatCreateRequest;
 import com.sparta.oishitable.domain.restaurantseat.entity.RestaurantSeat;
 import com.sparta.oishitable.domain.restaurantseat.repository.RestaurantSeatRepository;
-import com.sparta.oishitable.domain.seatType.entity.SeatType;
 import com.sparta.oishitable.domain.seatType.service.SeatTypeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.IntStream;
 
 @Service
 @Transactional(readOnly = true)
