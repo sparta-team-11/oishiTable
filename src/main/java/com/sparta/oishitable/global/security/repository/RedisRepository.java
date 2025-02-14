4b568adadb0575e030bd7fd6575d78c7d8bbb35e
SeuneKeun <eiziong1@gmail.com>
2025-02-14 20:32:25 +0900

    :recycle: refactor: redisRepository로 네이밍 수정

--- src/main/java/com/sparta/oishitable/global/security/repository/RedisRepository.java
+++ src/main/java/com/sparta/oishitable/global/security/repository/RedisRepository.java
@@ -1,19 +1,17 @@
-package com.sparta.oishitable.global.util;
+package com.sparta.oishitable.global.security.repository;
 
 import com.sparta.oishitable.global.exception.NotFoundException;
 import com.sparta.oishitable.global.exception.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RedisUtil {
+@RequiredArgsConstructor
+public class RedisRepository {
 
     private final RedisTemplate<String, String> redisTemplate;
-
-    public RedisUtil(RedisTemplate<String, String> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     public String getData(String key) {
         ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
