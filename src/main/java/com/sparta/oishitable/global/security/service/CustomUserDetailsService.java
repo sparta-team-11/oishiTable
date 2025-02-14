c2af6300ae4e922fffaec5bc2e016cff0a623948
SeuneKeun <eiziong1@gmail.com>
2025-02-14 11:19:08 +0900

    :sparkles: feat: UserDetail custom 객체 추가

---
+++ src/main/java/com/sparta/oishitable/global/security/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.sparta.oishitable.global.security.service;
+
+import com.sparta.oishitable.domain.user.entity.User;
+import com.sparta.oishitable.domain.user.repository.UserRepository;
+import com.sparta.oishitable.global.exception.CustomRuntimeException;
+import com.sparta.oishitable.global.exception.error.ErrorCode;
+import com.sparta.oishitable.global.security.entity.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomRuntimeException(ErrorCode.USER_NOT_FOUND));
+
+        return new CustomUserDetails(user);
+    }
+}
