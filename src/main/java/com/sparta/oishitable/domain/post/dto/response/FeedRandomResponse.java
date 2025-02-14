66bcb6cf2179d331392ba43d8b6896069d9f40de
gkdl4239 <gkdl4239@gmail.com>
2025-02-14 15:38:14 +0900

    :tada: add: Post 랜덤 조회시 게시글들과 커서값, 랜덤시드값, 다음페이지가 있는지 확인하는 boolean값을 포함한 response 생성

---
+++ src/main/java/com/sparta/oishitable/domain/post/dto/response/FeedRandomResponse.java
@@ -0,0 +1,12 @@
+package com.sparta.oishitable.domain.post.dto.response;
+
+import java.util.List;
+
+public record FeedRandomResponse(
+    List<PostRandomResponse> posts,
+    int randomSeed,
+    Long nextCursor,
+    boolean hasMore
+    ) {
+
+}
