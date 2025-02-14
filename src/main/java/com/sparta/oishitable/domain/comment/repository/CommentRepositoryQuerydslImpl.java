7bcea49b20c1bbd884c395b29383d49126256f6e
gkdl4239 <gkdl4239@gmail.com>
2025-02-14 23:32:28 +0900

    :sparkles: feat: 게시글 댓글 및 대댓글 조회 추가

---
+++ src/main/java/com/sparta/oishitable/domain/comment/repository/CommentRepositoryQuerydslImpl.java
@@ -0,0 +1,78 @@
+package com.sparta.oishitable.domain.comment.repository;
+
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.oishitable.domain.comment.dto.response.CommentResponse;
+import com.sparta.oishitable.domain.comment.entity.QComment;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryQuerydslImpl implements CommentRepositoryQuerydsl{
+
+    private final JPAQueryFactory queryFactory;
+    QComment comment = QComment.comment;
+
+    @Override
+    public List<CommentResponse> findPostComments(Long postId, Long cursorValue, int limit) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        builder.and(comment.post.id.eq(postId))
+            .and(comment.parent.isNull());
+
+        if (cursorValue != null) {
+            builder.and(comment.id.lt(cursorValue));
+        }
+
+        return queryFactory
+            .select(
+                Projections.constructor(CommentResponse.class,
+                    comment.id,
+                    comment.post.id,
+                    comment.user.id,
+                    comment.user.name,
+                    comment.content,
+                    comment.modifiedAt
+                    )
+            )
+            .from(comment)
+            .where(builder)
+            .orderBy(comment.createdAt.desc())
+            .limit(limit)
+            .fetch();
+    }
+
+    @Override
+    public List<CommentResponse> findReplies(Long parentCommentId, Long cursorValue, int limit) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (cursorValue != null) {
+            builder.and(comment.id.lt(cursorValue));
+        }
+
+        builder.and(comment.parent.id.eq(parentCommentId));
+
+        return queryFactory
+            .select(
+                Projections.constructor(CommentResponse.class,
+                    comment.id,
+                    comment.post.id,
+                    comment.user.id,
+                    comment.user.name,
+                    comment.content,
+                    comment.modifiedAt
+                    )
+            )
+            .from(comment)
+            .where(builder)
+            .orderBy(comment.createdAt.desc())
+            .limit(limit)
+            .fetch();
+    }
+}
