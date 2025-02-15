93a82298ea259c9d7811d117fc8df16b35e62a98
legoChoi <chlx159@gmail.com>
2025-02-16 04:38:59 +0900

    :sparkles: feat: 유저-가게 찜 삭제 서비스 로직

--- src/main/java/com/sparta/oishitable/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/sparta/oishitable/domain/bookmark/repository/BookmarkRepository.java
@@ -1,12 +1,8 @@
 package com.sparta.oishitable.domain.bookmark.repository;
 
 import com.sparta.oishitable.domain.bookmark.entity.Bookmark;
-import com.sparta.oishitable.domain.restaurant.entity.Restaurant;
-import com.sparta.oishitable.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
-    boolean existsByUserAndRestaurant(User user, Restaurant restaurant);
-
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     boolean existsByUser_IdAndRestaurant_Id(Long userId, Long restaurantId);
 }
