d39339091c03dfac696963a15d7753aae5b8ef25
gkdl4239 <gkdl4239@gmail.com>
2025-02-17 00:31:02 +0900

    :recycle: refactor: 조회 반환형 Slice 로 변경

--- src/main/java/com/sparta/oishitable/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/oishitable/domain/comment/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.sparta.oishitable.domain.comment.dto.response.CommentResponse;
 import com.sparta.oishitable.domain.comment.service.CommentService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,23 +30,23 @@ public class CommentController {
     }
 
     @GetMapping
-    public ResponseEntity<List<CommentResponse>> readPostComments(
+    public ResponseEntity<Slice<CommentResponse>> readPostComments(
         @RequestParam Long postId,
         @RequestParam(required = false) Long cursorValue,
         @RequestParam(defaultValue = "10") int limit
     ) {
-        List<CommentResponse> comments = commentService.getPostComments(postId, cursorValue, limit);
+        Slice<CommentResponse> comments = commentService.getPostComments(postId, cursorValue, limit);
 
         return ResponseEntity.ok(comments);
     }
 
     @GetMapping("/replies")
-    public ResponseEntity<List<CommentResponse>> readReplies(
+    public ResponseEntity<Slice<CommentResponse>> readReplies(
         @RequestParam Long commentId,
         @RequestParam(required = false) Long cursorValue,
         @RequestParam(defaultValue = "10") int limit
     ) {
-        List<CommentResponse> replies = commentService.getReplies(commentId, cursorValue, limit);
+        Slice<CommentResponse> replies = commentService.getReplies(commentId, cursorValue, limit);
 
         return ResponseEntity.ok(replies);
     }
