7530599d045c2c3f04a2485004c740705f06303d
gkdl4239 <gkdl4239@gmail.com>
2025-02-17 21:49:31 +0900

    :tada: add: 게시글 좋아요 서비스 생성
    좋아요 및 좋아요 취소 구현

---
+++ src/main/java/com/sparta/oishitable/domain/customer/post/like/service/PostLikeService.java
@@ -0,0 +1,68 @@
+package com.sparta.oishitable.domain.customer.post.like.service;
+
+import com.sparta.oishitable.domain.common.user.entity.User;
+import com.sparta.oishitable.domain.common.user.repository.UserRepository;
+import com.sparta.oishitable.domain.customer.post.entity.Post;
+import com.sparta.oishitable.domain.customer.post.like.entity.PostLike;
+import com.sparta.oishitable.domain.customer.post.like.repository.PostLikeRepository;
+
+import com.sparta.oishitable.domain.customer.post.repository.PostRepository;
+import com.sparta.oishitable.global.exception.CustomRuntimeException;
+import com.sparta.oishitable.global.exception.NotFoundException;
+import com.sparta.oishitable.global.exception.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PostLikeService {
+
+    private final UserRepository userRepository;
+    private final PostLikeRepository postLikeRepository;
+    private final PostRepository postRepository;
+
+    public void likePost(Long postId, Long userId) {
+
+        Post post = findPostById(postId);
+        User user = findUserById(userId);
+
+        if (postLikeRepository.existsByPostAndUser(post, user)) {
+            throw new CustomRuntimeException(ErrorCode.LIKE_DUPLICATED);
+        }
+
+        PostLike postLike = PostLike.builder()
+            .post(post)
+            .user(user)
+            .build();
+
+        postLikeRepository.save(postLike);
+    }
+
+    public void unlikePost(Long postId, Long userId) {
+
+        Post post = findPostById(postId);
+        User user = findUserById(userId);
+
+        PostLike postLike = findPostLikeByPostAndUser(post, user);
+
+        postLikeRepository.delete(postLike);
+    }
+
+    private PostLike findPostLikeByPostAndUser(Post post, User user) {
+        return postLikeRepository.findPostLikeByPostAndUser(post, user)
+            .orElseThrow(() -> new NotFoundException(ErrorCode.LIKE_NOT_FOUND));
+    }
+
+    private User findUserById(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private Post findPostById(Long commentId) {
+        return postRepository.findById(commentId)
+            .orElseThrow(() -> new NotFoundException(ErrorCode.POST_NOT_FOUND));
+    }
+
+}
