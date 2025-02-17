6a33324323b32314ef3b57c408abe59c62e7d6ad
gkdl4239 <gkdl4239@gmail.com>
2025-02-17 21:48:59 +0900

    :tada: add: 게시글 좋아요 엔티티 생성

---
+++ src/main/java/com/sparta/oishitable/domain/customer/post/like/entity/PostLike.java
@@ -0,0 +1,44 @@
+package com.sparta.oishitable.domain.customer.post.like.entity;
+
+import com.sparta.oishitable.domain.common.user.entity.User;
+import com.sparta.oishitable.domain.customer.post.entity.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post_likes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Builder
+    public PostLike(Long id, User user, Post post) {
+        this.id = id;
+        this.user = user;
+        this.post = post;
+    }
+}
