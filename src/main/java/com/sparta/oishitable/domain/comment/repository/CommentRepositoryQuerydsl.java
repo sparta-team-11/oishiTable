5421136970d388425b92292e1de1e855fdb3f361
gkdl4239 <gkdl4239@gmail.com>
2025-02-17 12:35:42 +0900

    :sparkles: feat: 대댓글까지 fetch join 해서 조회해오는 메서드 추가

--- src/main/java/com/sparta/oishitable/domain/comment/repository/CommentRepositoryQuerydsl.java
+++ src/main/java/com/sparta/oishitable/domain/comment/repository/CommentRepositoryQuerydsl.java
@@ -1,10 +1,13 @@
 package com.sparta.oishitable.domain.comment.repository;
 
 import com.sparta.oishitable.domain.comment.dto.response.CommentResponse;
+import com.sparta.oishitable.domain.comment.entity.Comment;
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepositoryQuerydsl {
 
     List<CommentResponse> findPostComments(Long postId, Long cursorValue, int limit);
     List<CommentResponse> findReplies(Long parentCommentId, Long cursorValue, int limit);
+    Optional<Comment> findCommentWithRepliesById(Long commentId);
 }
