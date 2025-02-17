b386060d9f05ad0f99011558c4e34c3813b2bde2
suho <sirius0991@gmail.com>
2025-02-17 10:24:32 +0900

    :recycle: refactor: remove index
    -불필요한 인덱스 제거
    -유니크 제약 조건 추가(중복 팔로우 방지)
    -외래키 이름 명시적 지정

--- src/main/java/com/sparta/oishitable/domain/follow/entity/Follow.java
+++ src/main/java/com/sparta/oishitable/domain/follow/entity/Follow.java
@@ -11,10 +11,8 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(
         name = "follows",
-        uniqueConstraints = {@UniqueConstraint(columnNames = {"follower_id", "following_id"})},
-        indexes = {
-                @Index(name = "idx_follower", columnList = "follower_id"),
-                @Index(name = "idx_following", columnList = "following_id")
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uq_follower_following", columnNames = {"follower_id", "following_id"})
         }
 )
 @Getter
@@ -27,11 +25,11 @@ public class Follow extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "follower_id", nullable = false)
+    @JoinColumn(name = "follower_id", nullable = false, foreignKey = @ForeignKey(name = "fk_follower"))
     private User follower;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "following_id", nullable = false)
+    @JoinColumn(name = "following_id", nullable = false, foreignKey = @ForeignKey(name = "fk_following"))
     private User following;
 
     @Builder
