98ca8c8281417441c582b46518ecd6cadbe5e01f
gkdl4239 <gkdl4239@gmail.com>
2025-02-17 21:49:20 +0900

    :tada: add: 게시글 좋아요 컨트롤러 생성
    좋아요 및 좋아요 취소 구현

---
+++ src/main/java/com/sparta/oishitable/domain/customer/post/like/controller/PostLikeController.java
@@ -0,0 +1,42 @@
+package com.sparta.oishitable.domain.customer.post.like.controller;
+
+import com.sparta.oishitable.domain.customer.post.like.service.PostLikeService;
+import com.sparta.oishitable.global.security.entity.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts/likes")
+@RequiredArgsConstructor
+public class PostLikeController {
+
+    private final PostLikeService postLikeService;
+
+    @PostMapping
+    public ResponseEntity<Void> likePost(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam Long postId
+
+    ) {
+        postLikeService.likePost(postId, user.getId());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> unlikePost(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @RequestParam Long postId
+
+    ) {
+        postLikeService.unlikePost(postId, user.getId());
+
+        return ResponseEntity.noContent().build();
+    }
+}
