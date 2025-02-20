b3d5a57a297f49e3fd304b101e68daf6de8ab6e8
gkdl4239 <gkdl4239@gmail.com>
2025-02-20 13:51:13 +0900

    :tada: add: 알림 레포지토리 생성

---
+++ src/main/java/com/sparta/oishitable/notification/repository/NotificationRepository.java
@@ -0,0 +1,16 @@
+package com.sparta.oishitable.notification.repository;
+
+import com.sparta.oishitable.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+
+    @Query("SELECT n FROM Notification n WHERE n.isSent = false AND n.scheduledTime <= :currentTime")
+    List<Notification> findDueNotifications(@Param("currentTime") LocalDateTime currentTime);
+}
