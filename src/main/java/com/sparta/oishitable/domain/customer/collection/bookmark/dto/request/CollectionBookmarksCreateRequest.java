37a80169a09c7d502c65db26698ba6cb88f8ad7b
legoChoi <chlx159@gmail.com>
2025-02-20 20:57:33 +0900

    :art: refactor: 컬렉션 북마크 등록 API valid 추가

--- src/main/java/com/sparta/oishitable/domain/customer/collection/bookmark/dto/request/CollectionBookmarksCreateRequest.java
+++ src/main/java/com/sparta/oishitable/domain/customer/collection/bookmark/dto/request/CollectionBookmarksCreateRequest.java
@@ -1,8 +1,11 @@
 package com.sparta.oishitable.domain.customer.collection.bookmark.dto.request;
 
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 public record CollectionBookmarksCreateRequest(
+        @Valid
         List<CollectionBookmarkCreateRequest> bookmarks
 ) {
 }
