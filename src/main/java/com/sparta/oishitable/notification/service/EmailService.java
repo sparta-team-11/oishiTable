04969610652bffc478f3a45f90c72ad464a6b984
gkdl4239 <gkdl4239@gmail.com>
2025-02-21 20:33:53 +0900

    :recycle: refactor: 발신 이메일 properties에서 가져오게 변경 / 메서드명 변경 / 예외처리 수정

--- src/main/java/com/sparta/oishitable/notification/service/EmailService.java
+++ src/main/java/com/sparta/oishitable/notification/service/EmailService.java
@@ -3,10 +3,13 @@ package com.sparta.oishitable.notification.service;
 import com.sparta.oishitable.domain.common.user.entity.User;
 import com.sparta.oishitable.domain.customer.reservation.entity.Reservation;
 import com.sparta.oishitable.domain.owner.restaurant.entity.Restaurant;
+import com.sparta.oishitable.global.exception.CustomRuntimeException;
+import com.sparta.oishitable.global.exception.error.ErrorCode;
 import com.sparta.oishitable.notification.entity.Notification;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -16,20 +19,22 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final JavaMailSender mailSender;
-    private final String SENDER = "oishitable52@gmail.com";
+
+    @Value(value = "${spring.mail.username}")
+    private String sender;
 
     public void sendEmail(Notification notification) {
 
         Reservation reservation = notification.getReservation();
         User user = notification.getUser();
-        String mailContent = getString(notification, reservation, user);
+        String mailContent = createMailContent(notification, reservation, user);
 
         try {
 
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-            helper.setFrom(SENDER);
+            helper.setFrom(sender);
             helper.setTo(user.getEmail());
             helper.setSubject("오이시 테이블 예약 알림");
             helper.setText(mailContent, true);
@@ -37,11 +42,11 @@ public class EmailService {
             mailSender.send(message);
 
         } catch (MessagingException e) {
-            e.printStackTrace();
+            throw new CustomRuntimeException(ErrorCode.EMAIL_SENDING_FAILED);
         }
     }
 
-    private static String getString(Notification notification, Reservation reservation, User user) {
+    private static String createMailContent(Notification notification, Reservation reservation, User user) {
         Restaurant restaurant = reservation.getRestaurantSeat().getRestaurant();
 
         // 공통 예약 정보
@@ -52,7 +57,7 @@ public class EmailService {
                         "<p><strong>예약 인원:</strong> %d</p>",
                 reservation.getDate(),
                 restaurant.getName(),
-                restaurant.getLocation(),
+                restaurant.getAddress(),
                 user.getName(),             // TODO : 예약 생성시 예약자명을 받아서 쓰게끔 차후 변경
                 reservation.getTotalCount()
         );
