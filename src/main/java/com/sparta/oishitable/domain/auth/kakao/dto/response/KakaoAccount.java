9c7d78a5341e0c4be1de8230469dd4a755b2ed22
gkdl4239 <gkdl4239@gmail.com>
2025-02-23 23:53:04 +0900

    :tada: add: KakaoAccount 생성

---
+++ src/main/java/com/sparta/oishitable/domain/auth/kakao/dto/response/KakaoAccount.java
@@ -0,0 +1,26 @@
+package com.sparta.oishitable.domain.auth.kakao.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class KakaoAccount {
+
+    private Profile profile;
+    private Boolean profileNicknameNeedsAgreement;
+
+    private String email;
+    private Boolean isEmailValid;
+    private Boolean isEmailVerified;
+    private Boolean hasEmail;
+    private Boolean emailNeedsAgreement;
+
+    private String phoneNumber;
+    private Boolean hasPhoneNumber;
+    private Boolean phoneNumberNeedsAgreement;
+
+}
