f1f038f419eaf6e7ac7ce8e75fd35f0b35a04213
gkdl4239 <gkdl4239@gmail.com>
2025-02-23 20:24:35 +0900

    :recycle: refactor: QueryProjection 사용을 위한 불변 클래스로 변경

--- src/main/java/com/sparta/oishitable/domain/customer/post/dto/response/PostRandomResponse.java
+++ src/main/java/com/sparta/oishitable/domain/customer/post/dto/response/PostRandomResponse.java
@@ -1,15 +1,40 @@
 package com.sparta.oishitable.domain.customer.post.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
-public record PostRandomResponse(
-        Long postId,
-        Long userId,
-        Long regionId,
-        String title,
-        String content,
-        String username,
-        LocalDateTime modifiedAt,
-        Long randomValue
-) {
+@Getter
+public class PostRandomResponse {
+
+    private final Long postId;
+    private final Long userId;
+    private final Long regionId;
+    private final String title;
+    private final String content;
+    private final String username;
+    private final LocalDateTime modifiedAt;
+    private final Long randomValue;
+
+    @QueryProjection
+    public PostRandomResponse(
+            Long postId,
+            Long userId,
+            Long regionId,
+            String title,
+            String content,
+            String username,
+            LocalDateTime modifiedAt,
+            Long randomValue
+    ) {
+        this.postId = postId;
+        this.userId = userId;
+        this.regionId = regionId;
+        this.title = title;
+        this.content = content;
+        this.username = username;
+        this.modifiedAt = modifiedAt;
+        this.randomValue = randomValue;
+    }
 }
