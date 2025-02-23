5d87ed144340fb367138dc4c953dbadab5c7f41a
gkdl4239 <gkdl4239@gmail.com>
2025-02-23 23:56:13 +0900

    :tada: add: OAuthToken 생성

---
+++ src/main/java/com/sparta/oishitable/domain/auth/kakao/dto/response/OAuthToken.java
@@ -0,0 +1,16 @@
+package com.sparta.oishitable.domain.auth.kakao.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+
+@Getter
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class OAuthToken {
+    private String accessToken;
+    private String tokenType;
+    private String refreshToken;
+    private int expiresIn;
+    private String scope;
+    private int refreshTokenExpiresIn;
+}
