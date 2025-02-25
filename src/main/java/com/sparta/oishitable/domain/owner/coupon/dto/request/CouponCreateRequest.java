db5bc29346d2236acfb01439631151cd7c5d09b5
zabastian <qlgh10gh@gmail.com>
2025-02-26 00:06:20 +0900

    :recycle: refactor: 주석 제거

--- src/main/java/com/sparta/oishitable/domain/owner/coupon/dto/request/CouponCreateRequest.java
+++ src/main/java/com/sparta/oishitable/domain/owner/coupon/dto/request/CouponCreateRequest.java
@@ -3,9 +3,6 @@ package com.sparta.oishitable.domain.owner.coupon.dto.request;
 import jakarta.validation.constraints.NotNull;
 
 public record CouponCreateRequest(
-//        @NotNull
-//        Long restaurantId,
-
         @NotNull
         Integer discount
 
