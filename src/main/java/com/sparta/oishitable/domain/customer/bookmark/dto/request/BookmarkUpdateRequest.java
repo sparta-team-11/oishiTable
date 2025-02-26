b6b3f98a20bc6e268dacda5c90045431241ebf54
legoChoi <chlx159@gmail.com>
2025-02-26 19:36:17 +0900

    :recycle: refactor: 북마크 메모 update 시 검증 변경
    - NotBlank -> NotNull : 메모는 없어도 되지만 빈 문자열, Null은 아님

--- src/main/java/com/sparta/oishitable/domain/customer/bookmark/dto/request/BookmarkUpdateRequest.java
+++ src/main/java/com/sparta/oishitable/domain/customer/bookmark/dto/request/BookmarkUpdateRequest.java
@@ -1,9 +1,9 @@
 package com.sparta.oishitable.domain.customer.bookmark.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record BookmarkUpdateRequest(
-        @NotBlank
+        @NotNull
         String updateMemo
 ) {
 }
