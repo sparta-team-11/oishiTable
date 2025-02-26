2cbd45e8852fdb33d056cf9b185a58e0ebcecda0
gkdl4239 <gkdl4239@gmail.com>
2025-02-26 09:29:32 +0900

    :recycle: refactor: @Valid 메세지 생성

--- src/main/java/com/sparta/oishitable/domain/customer/post/dto/request/PostCreateRequest.java
+++ src/main/java/com/sparta/oishitable/domain/customer/post/dto/request/PostCreateRequest.java
@@ -2,15 +2,18 @@ package com.sparta.oishitable.domain.customer.post.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record PostCreateRequest(
-        @NotNull
+        @NotNull(message = "지역을 선택 해주세요.")
         Long regionId,
 
-        @NotBlank
+        @NotBlank(message = "공백을 입력할 수 없습니다.")
+        @Size(max = 20, message = "최대 20자까지 입력 가능합니다.")
         String title,
 
-        @NotBlank
+        @NotBlank(message = "공백을 입력할 수 없습니다.")
+        @Size(max = 300, message = "최대 300자까지 입력 가능합니다.")
         String content
 ) {
 }
