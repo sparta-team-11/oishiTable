3c7674cbc3e8061acc6383dfac3920a1127f1a7e
legoChoi <chlx159@gmail.com>
2025-02-26 19:43:01 +0900

    :recycle: refactor: 컬렉션 정보 update 시 검증 추가
    - @NotBlank name : 컬렉션 명은 Null or 빈 문자열이어서는 안됌
    - @NotNull isPublic: 공개 여부는 Null 이어서는 안됌

--- src/main/java/com/sparta/oishitable/domain/customer/collection/dto/request/CollectionUpdateRequest.java
+++ src/main/java/com/sparta/oishitable/domain/customer/collection/dto/request/CollectionUpdateRequest.java
@@ -1,9 +1,16 @@
 package com.sparta.oishitable.domain.customer.collection.dto.request;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CollectionUpdateRequest(
+        @NotBlank
         String name,
+
         String description,
+
+        @NotNull
         Boolean isPublic
 ) {
 }
