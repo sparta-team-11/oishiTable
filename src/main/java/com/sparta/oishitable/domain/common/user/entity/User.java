c7e444b45eb797a0843afca117471394ca836908
SeuneKeun <eiziong1@gmail.com>
2025-02-26 20:36:40 +0900

    :recycle: refactor: 유저 이메일에 인덱스 추가

--- src/main/java/com/sparta/oishitable/domain/common/user/entity/User.java
+++ src/main/java/com/sparta/oishitable/domain/common/user/entity/User.java
@@ -12,7 +12,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_email", columnList = "email")
+        }
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
