95703f75c2a4d4518c99dfa3a0b636f2818d94b7
gkdl4239 <gkdl4239@gmail.com>
2025-02-27 01:15:02 +0900

    :recycle: refactor: 엔드포인트 변경

--- src/main/java/com/sparta/oishitable/domain/customer/post/like/controller/PostLikeController.java
+++ src/main/java/com/sparta/oishitable/domain/customer/post/like/controller/PostLikeController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/posts/likes")
+@RequestMapping("/customer/api/posts/likes")
 @RequiredArgsConstructor
 public class PostLikeController {
 
