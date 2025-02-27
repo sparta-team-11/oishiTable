04bf735c9bf7f19063c12b6efcdaece4eaa72319
legoChoi <chlx159@gmail.com>
2025-02-27 11:16:26 +0900

    :recycle: refactor: 응답 객체 record 변경

--- src/main/java/com/sparta/oishitable/domain/customer/collection/bookmark/dto/response/CollectionBookmarkDetails.java
+++ src/main/java/com/sparta/oishitable/domain/customer/collection/bookmark/dto/response/CollectionBookmarkDetails.java
@@ -1,29 +1,19 @@
 package com.sparta.oishitable.domain.customer.collection.bookmark.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.Getter;
 
-@Getter
-public class CollectionBookmarkDetails {
-
-    private final Long collectionBookmarkId;
-    private final Long restaurantId;
-    private final String bookmarkMemo;
-    private final String restaurantName;
-    private final String restaurantIntroduction;
-    private final String restaurantAddress;
-    private final Double longitude;
-    private final Double latitude;
+public record CollectionBookmarkDetails(
+        Long collectionBookmarkId,
+        Long restaurantId,
+        String bookmarkMemo,
+        String restaurantName,
+        String restaurantIntroduction,
+        String restaurantAddress,
+        Double longitude,
+        Double latitude
+) {
 
     @QueryProjection
-    public CollectionBookmarkDetails(Long collectionBookmarkId, Long restaurantId, String bookmarkMemo, String restaurantName, String restaurantIntroduction, String restaurantAddress, Double longitude, Double latitude) {
-        this.collectionBookmarkId = collectionBookmarkId;
-        this.restaurantId = restaurantId;
-        this.bookmarkMemo = bookmarkMemo;
-        this.restaurantName = restaurantName;
-        this.restaurantIntroduction = restaurantIntroduction;
-        this.restaurantAddress = restaurantAddress;
-        this.longitude = longitude;
-        this.latitude = latitude;
+    public CollectionBookmarkDetails {
     }
 }
--- src/main/java/com/sparta/oishitable/domain/customer/collection/dto/response/CollectionInfoResponse.java
+++ src/main/java/com/sparta/oishitable/domain/customer/collection/dto/response/CollectionInfoResponse.java
@@ -1,19 +1,14 @@
 package com.sparta.oishitable.domain.customer.collection.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.Getter;
 
-@Getter
-public class CollectionInfoResponse {
-
-    private final Long collectionId;
-    private final String name;
-    private final boolean isPublic;
+public record CollectionInfoResponse(
+        Long collectionId,
+        String name,
+        boolean isPublic
+) {
 
     @QueryProjection
-    public CollectionInfoResponse(Long collectionId, String name, boolean isPublic) {
-        this.collectionId = collectionId;
-        this.name = name;
-        this.isPublic = isPublic;
+    public CollectionInfoResponse {
     }
 }
