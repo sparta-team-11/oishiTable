db9b047b22f4421fce0f94967d4459f341722091
legoChoi <chlx159@gmail.com>
2025-03-05 20:46:02 +0900

    :art: refactor: Request 매개변수 명 변경

--- src/main/java/com/sparta/oishitable/domain/owner/restaurant/waiting/dto/request/WaitingQueueDeleteRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.oishitable.domain.owner.restaurant.waiting.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record WaitingQueueDeleteRequest(
-        @NotBlank
-        String waitingType
-) {
-}
